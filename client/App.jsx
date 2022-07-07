d2567ba985386b3569aec990e5516da18027d1c2
Eduardo Turcios <eddieturcios@outlook.com>
2022-07-07 17:38:39 -0400

    refac: moved chakra provider to main.js

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Routes from './Routes';
-import { ChakraProvider } from '@chakra-ui/react';
 
 export default function App() {
-  return (
-    <ChakraProvider>
-      <Routes />
-    </ChakraProvider>
-  );
+  return <Routes />;
 }
